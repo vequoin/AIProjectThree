8242cd32ced1d4f98aaa6737b638482bc0d3fc5d
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-12-10 00:04:44 -0500

    Update Generate_diagrams.py

--- Generate_diagrams.py
+++ Generate_diagrams.py
@@ -3,13 +3,8 @@ import random
 from PIL import Image
 import matplotlib.pyplot as plt
 
+
 def generate_diagram():
-    """
-    Generate a single 20x20 wiring diagram with the specified rules:
-    - 4 wires in total, each of a different color.
-    - Alternating between rows and columns.
-    - The diagram is dangerous if a red wire is laid before a yellow wire.
-    """
     grid_size = 20
     layout = np.zeros((grid_size, grid_size, 3), dtype=np.uint8)
     colors = [(255, 0, 0), (0, 0, 255), (255, 255, 0), (0, 255, 0)]  # Red, Blue, Yellow, Green in RGB
@@ -23,19 +18,21 @@ def generate_diagram():
         if i % 2 == 0:  # Row
             row = random.choice([r for r in range(grid_size) if r not in used_rows])
             used_rows.add(row)
+            if colors[i] == (255, 0, 0) and (255, 255, 0) in color_sequence:  # Check if Yellow is already placed
+                is_dangerous = True
             layout[row, :, :] = colors[i]
         else:  # Column
             column = random.choice([c for c in range(grid_size) if c not in used_columns])
             used_columns.add(column)
+            if colors[i] == (255, 0, 0) and (255, 255, 0) in color_sequence:  # Check if Yellow is already placed
+                is_dangerous = True
             layout[:, column, :] = colors[i]
 
         color_sequence.append(colors[i])
 
-        # Check if the diagram becomes dangerous
-        if colors[i] == (255, 0, 0):  # Red wire
-            is_dangerous = (255, 255, 0) in color_sequence  # Check if Yellow is already placed
-
     return layout, is_dangerous
+
+
 
 def save_diagrams(num_diagrams=1000):
     """
